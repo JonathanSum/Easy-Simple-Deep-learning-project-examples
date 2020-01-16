edb6eff8f345d557bfc7c2bde4eb3e76ec9621b3
unknown <777JonathanSum@gmail.com>
2020-01-16 05:39:07 -0800

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Easy-Simple-Deep-learning-project-examples
-These are super simple projects that anyone can write them in 20 mins or less.
-![GitHub Logo](https://github.com/JonathanSum/Easy-Simple-Deep-learning-project-examples/blob/master/download.png)
-
---
+++ precache-manifest.08f0afd5448bf23454da2fa4d5604956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814e8f79aa8ada1ab694e259afa1ab73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5869991b47ae62aad902",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c101b8c3a0117a861d2",
+    "url": "/static/js/2.760fda53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.760fda53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5869991b47ae62aad902",
+    "url": "/static/js/main.379101a1.chunk.js"
+  },
+  {
+    "revision": "71bf2302b1c5d11e94bb",
+    "url": "/static/js/runtime-main.6b032b25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
